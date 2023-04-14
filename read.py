0798df453bfb4cdad5b392bfcbd95e0a0b809a4c
宋超 <sc@chainshome.org>
2023-04-14 21:33:06 +0800

    fix some about read test

--- read.py
+++ read.py
@@ -25,17 +25,21 @@ cursor = db.cursor()
 
 
 # for loop
-pageSize = 2000000
-j = 0
+pageSize = 1000000
+j = 3000000
 while True:
     sql = "SELECT * FROM class_table order by class_name limit " + str(pageSize) + " offset " + str(j) + ";"
+#    sql = "SELECT * FROM class_table limit " + str(j) + ";"
     qst = time.time()
     cursor.execute(sql)
-    allrows = cursor.fetchall()
     qet = time.time()
-    if len(allrows) > 0:
+    allrows = cursor.fetchall()
+    fet = time.time()
+    if len(allrows) < 100000000:
+        print('offset:' + str(j) + ', page size:' + str(pageSize))
+        print('execute time is ' + str(qet - qst) + 's and fetch time is ' + str(fet - qet) + 's.')
         j += pageSize
-        print('query time is ' + str(qet - qst) + 's.')
+        time.sleep(3)
     else:
         break
 # db.commit()        # it is faster than put this in the InsertData method
