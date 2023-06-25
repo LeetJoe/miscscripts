5db2845ea64dec691a13f046b1df362eb492b871
LeetJoe <tjusongchao@outlook.com>
2023-06-25 18:16:47 +0800

    add some

--- esearch/esApi.py
+++ esearch/esApi.py
@@ -4,8 +4,8 @@ from config import es as esconf
 
 # Single node via URL
 es = Elasticsearch(
-    esconf.host,
-    basic_auth=(esconf.username, esconf.password)
+    esconf['host'],
+    basic_auth=(esconf['username'], esconf['password'])
 )
 
 '''
@@ -27,3 +27,90 @@ es = Elasticsearch([
 
 '''
 
+def create_index(client, index_name):
+    """Creates an index in Elasticsearch if one isn't already there."""
+    client.indices.create(
+        index=index_name,
+        body={
+            "settings": {"number_of_shards": 1},
+            "mappings": {
+                "properties": {
+                    "from": {"type": "text"},
+                    "relation": {"type": "keyword"},
+                    "to": {"type": "text"}
+                }
+            },
+        },
+        ignore=400,
+    )
+
+
+import datetime
+def index():
+    doc = {
+        'author': 'author_name',
+        'text': 'Interensting content...',
+        'timestamp': datetime.now(),
+    }
+    resp = es.index(index="test-index", id=1, document=doc)
+    # print(resp['result'])
+    return resp
+
+def getdoc():
+    resp = es.get(index="test-index", id=1)
+    # print(resp['_source'])
+    return resp
+
+def refresh_index():
+    es.indices.refresh(index="test-index")
+
+def query_simple():
+    resp = es.search(index="test-index", query={"match_all": {}})
+    # print("Got %d Hits:" % resp['hits']['total']['value'])
+    # for hit in resp['hits']['hits']:
+        # print("%(timestamp)s %(author)s: %(text)s" % hit["_source"])
+
+    return resp
+
+def update_simple(id, doc):
+    resp = es.update(index="test-index", id=id, document=doc)
+    return resp
+
+def delete_by_id(id):
+    es.delete(index="test-index", id=id)
+
+
+import csv
+from elasticsearch.helpers import streaming_bulk
+
+def bulk_index(client, csv_data_path):
+    def generate_actions(f):
+        reader = csv.DictReader(f)
+        for row in reader:
+            doc = {
+                "from": row["from"],
+                "relation": '惩罚',
+                "to": row["to"],
+            }
+            # print(doc)
+            yield doc
+
+    f = open(csv_data_path, mode="r")
+    print("Indexing documents...")
+    # generate_actions(f)
+
+    successes = 0
+    total_num = 0
+
+    for ok, action in streaming_bulk(
+        client=client, index="elasicsearch-kg-test", actions=generate_actions(f),
+    ):
+        print(ok)
+        print(action)
+        successes += ok
+        total_num += 1
+
+    print("Indexed %d/%d documents" % (successes, total_num))
+
+# create_index(es, 'elasicsearch-kg-test')
+# bulk_index(es, '/Users/neosong/Work/code/git/ucasai/miscscripts/downloads/edge_chengfa.csv')
