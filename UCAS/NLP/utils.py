5d1e7b375d251ea9cd348ebba2ce4068b74a17e1
LeetJoe <tjusongchao@outlook.com>
2023-12-19 13:15:24 +0800

    fix utils.py

--- UCAS/NLP/utils.py
+++ UCAS/NLP/utils.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import replicate
 
 from elasticsearch import Elasticsearch
 from config import es as esconf
@@ -21,41 +20,3 @@ def json_send(url, data=None, extra_headers=None, method="POST"):
         response = requests.get(url=url, headers=headers)
     return response.json()
 
-
-def json_send_sea(query):
-    es = Elasticsearch(
-        esconf['host'],
-        basic_auth=(esconf['username'], esconf['password'])
-    )
-
-    resp = es.search(
-        index=esconf['index'],
-        size=60,
-        query={
-            "function_score": {
-                "query": {
-                    "bool": {
-                        "must": {
-                            "combined_fields": {
-                                "query": query,
-                                "fields": ["from", "relation", "to"]
-                            }
-                        }
-                    }
-                },
-                "min_score": 20
-            }
-        },
-        filter_path=[
-            'hits.hits._score',
-            'hits.hits._source.from',
-            'hits.hits._source.relation',
-            'hits.hits._source.to',
-        ]
-    )
-
-    if not 'hits' in resp.keys():
-        return []
-    if not 'hits' in resp['hits'].keys():
-        return []
-    return [hit['_source']['from'] + hit['_source']['relation'] + hit['_source']['to'] for hit in resp['hits']['hits']]
