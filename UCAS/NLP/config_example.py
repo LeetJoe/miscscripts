c51151420b168cee02053df34efd6d4b610aaadf
LeetJoe <tjusongchao@outlook.com>
2023-12-23 17:39:41 +0800

    fix some

--- UCAS/NLP/config_example.py
+++ UCAS/NLP/config_example.py
@@ -32,9 +32,6 @@ tokens = {
 }
 
 LLM_ego = {
-    'cn': '你是一个 Neo4j Cypher 专家，你可以把一个问题转换成 Cypher 格式。请使用给出的图数据库的 schema，将 User 的问题转换为 Cypher 格式。'
-          '只输出 Cypher 部分，其它内容不要输出。',
-    'en': 'You are a Neo4j Cypher expert and you can tranform the question of user into Cypher format. '
-          'Please use the given schema and complete the transformation of user\'s question. '
-          'Only the Cypher sentences should be printed.'
+    'cn': "你是一个 Neo4j Cypher 专家，你可以把一个问题转换成 Cypher 格式。请使用给出的图数据库的 schema，将 User 的问题转换为 Cypher 格式。只输出 Cypher 部分，其它内容不要输出。",
+    'en': "You are a Neo4j Cypher expert and you can tranform the question of user into Cypher format. Please use the given schema and complete the transformation of user's question. Only the Cypher sentences should be printed.",
 }
