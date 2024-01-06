0c2519a00e62b70f4b364ea1ff0c4cc974e2ff02
LeetJoe <tjusongchao@outlook.com>
2024-01-06 13:54:53 +0800

    fix some

--- UCAS/PRML/homework/svm.py
+++ UCAS/PRML/homework/svm.py
@@ -5,7 +5,9 @@ import sklearn.datasets._samples_generator as sk_sample_generator
 import sklearn.model_selection as sk_model_selection
 import sklearn.preprocessing as sk_preprocessing
 
-import sklearn.svm as sk_svm
+from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
 
 
 digitals = sk_datasets.load_digits()
@@ -13,12 +15,6 @@ digitals_X = digitals.data
 digitals_y = digitals.target
 
 X, y = sk_sample_generator.make_classification(n_samples=6,n_features=5,n_informative=2,n_redundant=3,n_classes=2,n_clusters_per_class=2,scale=1,random_state=20)
-'''
-for x_,y_ in zip(X,y):
-    print(y_,end=": ")
-    print(x_)
-'''
-
 X_train, X_test, y_train, y_test = sk_model_selection.train_test_split(digitals_X, digitals_y, train_size=0.8, random_state=20)
 
 scaler = sk_preprocessing.StandardScaler().fit(X)
@@ -29,11 +25,14 @@ new_X=scaler.transform(X)  # scale into [0, 1]
 
 new_X = sk_preprocessing.normalize(X,norm='l2')  # L2 normalization
 
-model = sk_svm.SVC(C=1.0, kernel='rbf', gamma='auto')
-model.fit(X_train,y_train)
-acc = model.score(X_test,y_test)  # 计算正确率的均值
-print('model score: ',acc)
+parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
+svc = svm.SVC()
+model = GridSearchCV(svc, parameters)
+model.fit(digitals.data, digitals.target)
 
-predictions = model.predict(X_test)
+scores = cross_val_score(model, digitals_X, digitals_y, cv=5)
+print("平均正确率: ", scores.mean())
 
-print(predictions)
+# 测试集上的预测
+# predictions = model.predict(X_test)
+# print(predictions)
