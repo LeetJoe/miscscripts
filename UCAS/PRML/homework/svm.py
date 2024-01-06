a4a3d45205089e89c5b582180141b76e1984e2e0
LeetJoe <tjusongchao@outlook.com>
2024-01-06 14:06:48 +0800

    fix some

--- UCAS/PRML/homework/svm.py
+++ UCAS/PRML/homework/svm.py
@@ -1,5 +1,3 @@
-import joblib
-
 import sklearn.datasets as sk_datasets
 import sklearn.datasets._samples_generator as sk_sample_generator
 import sklearn.model_selection as sk_model_selection
@@ -11,28 +9,17 @@ from sklearn.model_selection import cross_val_score
 
 
 digitals = sk_datasets.load_digits()
-digitals_X = digitals.data
-digitals_y = digitals.target
 
-X, y = sk_sample_generator.make_classification(n_samples=6,n_features=5,n_informative=2,n_redundant=3,n_classes=2,n_clusters_per_class=2,scale=1,random_state=20)
-X_train, X_test, y_train, y_test = sk_model_selection.train_test_split(digitals_X, digitals_y, train_size=0.8, random_state=20)
-
-scaler = sk_preprocessing.StandardScaler().fit(X)
-new_X = scaler.transform(X)  # 基于mean和std的标准化
- 
-scaler = sk_preprocessing.MinMaxScaler(feature_range=(0,1)).fit(X)
-new_X=scaler.transform(X)  # scale into [0, 1]
-
-new_X = sk_preprocessing.normalize(X,norm='l2')  # L2 normalization
+X_train, X_test, y_train, y_test = sk_model_selection.train_test_split(digitals.data, digitals.target, train_size=0.8,
+                                                                       random_state=20)
 
 parameters = {'kernel': ('linear', 'rbf'), 'C': [1, 10]}
 svc = svm.SVC()
 model = GridSearchCV(svc, parameters)
-model.fit(digitals.data, digitals.target)
+model.fit(X_train, y_train)
 
-scores = cross_val_score(model, digitals_X, digitals_y, cv=5)
-print("平均正确率: ", scores.mean())
+scores = cross_val_score(model, X_train, y_train, cv=5)
+print("train score: ", scores.mean())
 
-# 测试集上的预测
-# predictions = model.predict(X_test)
-# print(predictions)
+scores = cross_val_score(model, X_test, y_test, cv=5)
+print("test score: ", scores.mean())
