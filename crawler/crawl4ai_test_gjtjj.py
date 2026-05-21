d23287af758d7019ff1a36f598f771f141c23cb5
LeetJoe <tjusongchao@outlook.com>
2026-05-22 02:12:17 +0800

    add some

--- crawler/crawl4ai_test_gjtjj.py
+++ crawler/crawl4ai_test_gjtjj.py
@@ -1,5 +1,5 @@
 import asyncio
-from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode
+from crawl4ai import AsyncWebCrawler, BrowserConfig, CrawlerRunConfig, CacheMode, DefaultTableExtraction
 from crawl4ai.content_filter_strategy import PruningContentFilter
 from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
 
@@ -22,22 +22,23 @@ async def main():
         # CacheMode.ENABLED, CacheMode.DISABLED, CacheMode.BYPASS
         cache_mode=CacheMode.BYPASS,
 
-        ## comma seperated selectors for multiple areas
-        # css_selector="div.rich_media_area_primary_inner > #img-content",
-        # excluded_selector=".code-block-extension-header",
+        css_selector="div.wrapper-content > div.detail-content",
+        excluded_selector=".detail-title-des",
 
         # add markdown generator
         markdown_generator=md_generator,
+
+        # table_extraction=DefaultTableExtraction(table_score_threshold=-3)
     )
 
     async with AsyncWebCrawler(config=browser_conf) as crawler:
         result = await crawler.arun(
-            url="https://mp.weixin.qq.com/s/M91es9R9_RkZ1zsM6FPsWA",
+            url="https://www.stats.gov.cn/sj/zxfb/202605/t20260518_1963732.html",
             config=run_conf,
         )
 
-        # print(result.markdown.raw_markdown)
-        print(result.html)
+        print(result.markdown.raw_markdown)
+        # print(result.html)
 
 
 
